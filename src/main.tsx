48f710f7b7569a154e018afe66e4c0a0d10296c3
Evan <evanaltwarg2021@gmail.com>
2024-01-08 17:38:13 -0500

    Fix base url

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { Router, RouterProvider } from "@tanstack/react-router";
+import {
+  Router,
+  RouterProvider,
+  createMemoryHistory,
+} from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
-
-const router = new Router({ routeTree, defaultPreload: "intent" });
-
+const memoryHistory = createMemoryHistory({
+  initialEntries: ["/"],
+});
+const router = new Router({
+  routeTree,
+  defaultPreload: "intent",
+  history: memoryHistory,
+});
 declare module "@tanstack/react-router" {
   interface Register {
     router: typeof router;
